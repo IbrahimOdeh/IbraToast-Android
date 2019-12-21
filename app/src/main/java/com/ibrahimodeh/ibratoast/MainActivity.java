783b34b1b31757631604be4d90c348eaa2d2c076
Ibrahim Odeh <IbrahimOdeh@users.noreply.github.com>
2019-12-22 01:44:58 +0200

    Second Commit
    Second Commit.

--- app/src/main/java/com/ibrahimodeh/ibratoast/MainActivity.java
+++ app/src/main/java/com/ibrahimodeh/ibratoast/MainActivity.java
@@ -2,12 +2,53 @@ package com.ibrahimodeh.ibratoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.ibrahimodeh.ibratoastlib.IbraToast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button Success, Error, Info, Dark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Success = (Button) findViewById(R.id.SuccessToast);
+        Error = (Button) findViewById(R.id.ErrorToast);
+        Info = (Button) findViewById(R.id.InfoToast);
+        Dark = (Button) findViewById(R.id.DarkSToast);
+
+
+        Success.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IbraToast.makeText(MainActivity.this, "Success !", Toast.LENGTH_SHORT, 1).show();
+            }
+        });
+
+        Error.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IbraToast.makeText(MainActivity.this, "Error !", Toast.LENGTH_SHORT, 2).show();
+            }
+        });
+
+        Info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IbraToast.makeText(MainActivity.this, "Info", Toast.LENGTH_SHORT, 3).show();
+            }
+        });
+
+        Dark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IbraToast.makeText(MainActivity.this, "Dark Toast", Toast.LENGTH_SHORT, 4).show();
+            }
+        });
     }
 }
